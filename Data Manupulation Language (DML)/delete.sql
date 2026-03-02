40a434cbae8a9455a2319211dba7695e7ee6d1c9
Furkan KALKAN <156606690+Furkan9534@users.noreply.github.com>
2026-03-02 21:33:17 +0300

    SQL

--- Data Manupulation Language (DML)/delete.sql
+++ Data Manupulation Language (DML)/delete.sql
@@ -1,17 +1,17 @@
 # SQL DELETE
 The DELETE statement delete existing records in a table. DELETE FROM table_name WHERE condition;
 
-## Example 
+# Example 
 
 DELETE FROM Product WHERE ProductName='Tea';
 
 
----Delete All Records
----DELETE FROM table_name;
+# Delete All Records
+# DELETE FROM table_name;
 DELETE FROM ProductName;
 
----Delete a Table
---- You should use DROP TABLE
+# Delete a Table
+# You should use DROP TABLE
 
----Example
+# Example
 DROP TABLE ProductName;
